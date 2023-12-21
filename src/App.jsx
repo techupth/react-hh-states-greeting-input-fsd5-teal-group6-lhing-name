bf9ef1fe28466a628e9ec5f7783d50b2ef1d3ee1
Lhing <thorntan.r@gmail.com>
2023-12-21 14:08:52 +0700

    feat: completed react state exercise(2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message"); // ใช้เปลี่ยนค่า greeting-container
+  const [newMessage, setNewMessage] = useState(""); // ใช้รับค่าใน input
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setNewMessage(event.target.value);
+          }}
+          value={newMessage}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setGreetingMessage(newMessage);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
